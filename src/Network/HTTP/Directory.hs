juhp/http-directory

commit a447aa30ba52f65a865d836dcdf7ba27e4b723a3

remove absolute self-url prefix and filter :?/ infix
- uses network-uri
- fixes google.com sporadic: preferences?lang=en ads/en
- error'

http-directory.cabal
@@ -1,6 +1,6 @@
 cabal-version:       1.18
 name:                http-directory
-version:             0.1.5
+version:             0.1.6
 synopsis:            http directory listing library
 description:
             Library for listing the files (href's) in an http directory.
@@ -38,6 +38,7 @@ library
                        http-client-tls,
                        http-date >= 0.0.7,
                        http-types,
+                       network-uri,
                        text,
                        time,
                        xml-conduit

src/Network/HTTP/Directory.hs
@@ -42,21 +42,25 @@ import Control.Monad (when)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.List as L
 import Data.Maybe
-import Data.Text (Text, isPrefixOf, isInfixOf)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Time.Clock (UTCTime)
 
 import Network.HTTP.Client (hrRedirects, httpLbs, httpNoBody, Manager, method,
-                            newManager, parseRequest,
-                            Request, Response, responseBody, responseHeaders,
+                            newManager, parseRequest, Request,
+                            Response, responseBody, responseHeaders,
                             responseOpenHistory, responseStatus)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.Date (httpDateToUTC, parseHTTPDate)
 import Network.HTTP.Types (hContentLength, hLocation, methodHead, statusCode)
+import Network.URI (parseURI, URI(..))
 
 import Text.HTML.DOM (parseLBS)
 import Text.XML.Cursor
 
--- | List the file links (hrefs) in an http directory
+-- | List the files (hrefs) in an http directory
+--
+-- It filters out absolute urls & paths, queries, '..', and '#' links.
 --
 -- Raises an error if the http request fails.
 --
@@ -65,16 +69,30 @@ import Text.XML.Cursor
 -- the actual final url prefix for relative links
 -- (files).
 --
--- (since 0.1.4 it filters "non-files/subdirs" -
---  before that it was just httpRawDirectory)
+-- (before 0.1.4 it was just httpRawDirectory)
 httpDirectory :: Manager -> String -> IO [Text]
 httpDirectory mgr url = do
   hrefs <- httpRawDirectory mgr url
-  return $ L.nub $ filter (not . or . flist (map isPrefixOf ["/","?"] ++ [(`elem` ["../", "..", "#"]), (":" `isInfixOf`)])) hrefs
+  return $ defaultFilesFilter uri hrefs
+  where
+    uri = parseURI url
 
--- picked from swish
-flist :: [a->b] -> a -> [b]
-flist fs a = map ($ a) fs
+defaultFilesFilter :: Maybe URI -> [Text] -> [Text]
+defaultFilesFilter mUri =
+  L.nub . filter (not . or . flist ((map T.isInfixOf [":", "?", "/"]) ++ [(`elem` ["../", "..", "#"])])) . map removePath
+  where
+    -- picked from swish
+    flist :: [a->b] -> a -> [b]
+    flist fs a = map ($ a) fs
+
+    removePath :: Text -> Text
+    removePath t =
+      case mpath of
+        Nothing -> t
+        Just path ->
+          fromMaybe t $ T.stripPrefix (T.pack path) t
+
+    mpath = uriPath <$> mUri
 
 -- | Like httpDirectory but uses own Manager
 --
@@ -135,11 +153,12 @@ httpLastModified mgr url = do
       mdate = lookup "Last-Modified" headers
   return $ httpDateToUTC <$> maybe Nothing parseHTTPDate mdate
 
+-- conflicts with Request
 checkResponse :: String -> Response r -> IO ()
 checkResponse url response =
   when (statusCode (responseStatus response) /= 200) $ do
     putStrLn url
-    error $ show $ responseStatus response
+    error' $ show $ responseStatus response
 
 -- | alias for 'newManager tlsManagerSettings'
 -- so one does not need to import http-client etc
@@ -186,3 +205,10 @@ httpHead mgr url = do
 isHttpUrl :: String -> Bool
 isHttpUrl loc = "http:" `L.isPrefixOf` loc || "https:" `L.isPrefixOf` loc
 
+-- from simple-cmd
+error' :: String -> a
+#if (defined(MIN_VERSION_base) && MIN_VERSION_base(4,9,0))
+error' = errorWithoutStackTrace
+#else
+error' = error
+#endif

commit f6caeb8255fc8528722040fcaf541c128fe80c96

filter '/' only if nonSuffix!
fixes regression introduced by filtering '/' infix (introduced by a447aa3) :-(

Add a httpDirectory' test on fedora releases to check for this.

http-directory.cabal
@@ -1,6 +1,6 @@
 cabal-version:       1.18
 name:                http-directory
-version:             0.1.7
+version:             0.1.8
 synopsis:            http directory listing library
 description:
             Library for listing the files (href's) in an http directory.

src/Network/HTTP/Directory.hs
@@ -81,7 +81,7 @@ httpDirectory mgr url = do
 
 defaultFilesFilter :: Maybe URI -> [Text] -> [Text]
 defaultFilesFilter mUri =
-  L.nub . filter (not . or . flist (map T.isInfixOf [":", "?", "/"] ++ [(`elem` ["../", "..", "#"])])) . map removePath
+  L.nub . filter (not . or . flist (map T.isInfixOf [":", "?"] ++ [nonTrailingSlash] ++ [(`elem` ["../", "..", "#"])])) . map removePath
   where
     -- picked from swish
     flist :: [a->b] -> a -> [b]
@@ -89,13 +89,14 @@ defaultFilesFilter mUri =
 
     removePath :: Text -> Text
     removePath t =
-      case mpath of
+      case fmap uriPath mUri of
         Nothing -> t
         Just path ->
           fromMaybe t $ T.stripPrefix (T.pack path) t
 
-    mpath = uriPath <$> mUri
-
+    nonTrailingSlash :: Text -> Bool
+    nonTrailingSlash t =
+      "/" `T.isInfixOf` T.init t
 -- | Like httpDirectory but uses own Manager
 --
 -- @since 0.1.4

test/Spec.hs
@@ -19,6 +19,10 @@ spec = do
       fs <- httpDirectory mgr "http://httpbin.org/"
       fs `shouldBe` []
 
+    it "fedora releases" $ do
+      fs <- httpDirectory' "https://dl.fedoraproject.org/pub/fedora/linux/releases/"
+      null fs `shouldBe` False
+
     it "my src hackage" $ do
       fs <- httpDirectory' "https://hackage.haskell.org/package/http-directory/src/"
       null fs `shouldBe` False